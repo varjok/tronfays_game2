0f15f5ece053ea61256a22b68e920c9c502e2cfe
Varvara <71887939+varjok@users.noreply.github.com>
2020-09-25 19:09:40 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+function bg_music () {
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(494, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+}
+input.buttonA.onEvent(ButtonEvent.Click, function () {
+    makerController.player1.press(ArcadeButton.Left)
+})
+input.buttonB.onEvent(ButtonEvent.Click, function () {
+    makerController.player1.press(ArcadeButton.RightUp)
+})
 forever(function () {
 	
 })
