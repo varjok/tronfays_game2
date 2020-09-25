a724c89385baf6b1fa50525918c235e3bd23b96f
jocelym <43392541+jocelym@users.noreply.github.com>
2020-09-25 12:08:16 -0400

    added light functions

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+input.buttonA.onEvent(ButtonEvent.Click, function () {
+    light.showRing(
+    `blue purple blue purple blue black black black black black`
+    )
+})
+input.onGesture(Gesture.Shake, function () {
+    light.showAnimation(light.rainbowAnimation, 1000)
+})
+input.buttonB.onEvent(ButtonEvent.Click, function () {
+    light.showRing(
+    `black black black black black pink yellow pink yellow pink`
+    )
+})
+light.setAll(0x00ff00)
 forever(function () {
 	
 })
